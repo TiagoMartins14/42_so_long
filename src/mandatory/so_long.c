a2afca10073558bffbbc5e4e463f7cce1454b92d
TiagoMartins14 <92262694+TiagoMartins14@users.noreply.github.com>
2023-11-16 13:10:30 +0000

    Experiemnting with mlx
    Able to create a rainbow effect.

--- src/mandatory/so_long.c
+++ src/mandatory/so_long.c
@@ -6,7 +6,7 @@
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 08:59:47 by tiaferna          #+#    #+#             */
-/*   Updated: 2023/11/15 11:58:20 by tiaferna         ###   ########.fr       */
+/*   Updated: 2023/11/16 13:07:08 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ int	get_b(int trgb)
 	return (trgb & 0xFF);
 }
 
-int shade_trgb(int color, double shade)
+int shade_trgb(int color, double shade) //not wrking correctly yet
 {
 	int	shade_color;
 
@@ -76,47 +76,139 @@ int shade_trgb(int color, double shade)
 	return (shade_color);
 }
 
+int	close_win(int keycode, t_vars *vars)
+{
+	ft_printf("%d\n", keycode);
+	if (keycode == 65307)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit (0);
+	}
+	return (0);
+}
+
+int	mouse_enter_hook(void)
+{
+	static int count = 1;
+	printf("%d\n", count++);
+	return (0);
+}
+
+void drawCircle(t_data img, int xc, int yc, int x, int y) 
+{ 
+	
+    my_mlx_pixel_put(&img, xc+x, yc+y, 255); 
+    my_mlx_pixel_put(&img, xc-x, yc+y, 255); 
+    my_mlx_pixel_put(&img, xc+x, yc-y, 255); 
+    my_mlx_pixel_put(&img, xc-x, yc-y, 255); 
+    my_mlx_pixel_put(&img, xc+y, yc+x, 255); 
+    my_mlx_pixel_put(&img, xc-y, yc+x, 255); 
+    my_mlx_pixel_put(&img, xc+y, yc-x, 255); 
+    my_mlx_pixel_put(&img, xc-y, yc-x, 255); 
+} 
+  
+// Function for circle-generation 
+// using Bresenham's algorithm 
+void circleBres(t_data img, int xc, int yc, int r) 
+{
+    int x = 0, y = r; 
+    int d = 3 - 2 * r; 
+    drawCircle(img, xc, yc, x, y); 
+    while (y >= x) 
+    { 
+        // for each pixel we will 
+        // draw all eight pixels 
+          
+        x++; 
+  
+        // check for decision parameter 
+        // and correspondingly  
+        // update d, x, y 
+        if (d > 0) 
+        { 
+            y--;  
+            d = d + 4 * (x - y) + 10; 
+        } 
+        else
+            d = d + 4 * x + 6; 
+        drawCircle(img, xc, yc, x, y); 
+        //delay(50); 
+    } 
+} 
+
+int	move_right(int keycode, t_vars vars, t_data img, int xc, int yc, int r)
+{
+	ft_printf("%d\n", keycode);
+	if (keycode ==  100)
+	{
+		circleBres(img, ++xc, ++yc, r);
+		mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	}
+	return (0);
+}
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
 	t_data	img;
+	t_vars	vars;
 	int	x;
 	int	y;
 	int	reset_x;
-	int color;
-
-	x = 200;
-	y = 200;
+	int reset_y;
+	int color;	
+	x = 0;
+	y = 0;
 	reset_x = x;
+	reset_y = y;
 	color = create_trgb(0, 255, 0, 0);
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 500, 500, "so_long");
-	img.img = mlx_new_image(mlx, 500, 500);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 500, 500, "so_long");
+	img.img = mlx_new_image(vars.mlx, 500, 500);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-	while (y <= 300)
+	// int	red = 255;
+	// int	green = 0;
+	// int	blue = 0;
+	/* while(1)
 	{
-		color = create_trgb(0, 255, 0, 0);
-		x = reset_x;
-		while (x <= 300)
+		y = reset_y;
+		while (y <= 500)
 		{
-			color = shade_trgb(color, 1);
-			if (x> 210)
-				color = shade_trgb(color, 0.8);
-			if (x > 225)
-				color = shade_trgb(color, 0.6);
-			if (x > 250)
-				color = shade_trgb(color, 0.4);
-			if (x > 275)
-				color = shade_trgb(color, 0.2);
-			if (x > 290)
-				color = shade_trgb(color, 0.0);
-			my_mlx_pixel_put(&img, x, y, color);
-			mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-			x++;
+			x = reset_x;
+			while (x <= 500)
+			{
+				color = create_trgb(0, red, green, blue);
+				my_mlx_pixel_put(&img, x, y, color);
+				x++;
+			}
+			y++;
 		}
-		y++;
-	}
-	mlx_loop(mlx);
+		if (red > 0 && blue == 0)
+		{
+			green++;
+			red--;
+		}
+		if (green > 0 && red == 0)
+		{
+			blue++;
+			green--;
+		}
+		if (blue > 0 && green == 0)
+		{
+			red++;
+			blue--;
+		}
+		mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+		usleep(100000);
+		ft_printf("%d %d %d\n", red, green, blue);
+	} */
+	int xc = 250;
+	int yc = 250;
+	int r = 200;
+	circleBres(img, xc, yc, r);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_hook(vars.win, 2, 1L<<0, move_right, &vars);
+	//mlx_hook(vars.win, 2, 1L<<0, close_win, &vars);
+	mlx_hook(vars.win, 6, 1L<<6, mouse_enter_hook, &vars);
+	mlx_loop(vars.mlx);
 }
--- src/mandatory/so_long.h
+++ src/mandatory/so_long.h
@@ -6,7 +6,7 @@
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 08:59:13 by tiaferna          #+#    #+#             */
-/*   Updated: 2023/11/15 11:30:39 by tiaferna         ###   ########.fr       */
+/*   Updated: 2023/11/16 11:55:35 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 # include "../libft/libft.h"
 # include "../libft/ft_printf/ft_printf.h"
 // # include "../libft/get_next_line/get_next_line.h"
@@ -28,6 +29,12 @@ typedef struct s_data
 	int		line_length;
 	int		endian;
 }	t_data;
+
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+}	t_vars;
 
 // int	is_map_rectangular(int	fd_map);
 
