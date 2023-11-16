44a14f96a089577a272da032302dca20110bb569
TiagoMartins14 <92262694+TiagoMartins14@users.noreply.github.com>
2023-11-16 23:23:35 +0000

    Further experiements with mlx
    I'm now able to move the circle inside the window using the WASD keys.

--- src/mandatory/so_long.c
+++ src/mandatory/so_long.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
+/*   By: patatoss <patatoss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 08:59:47 by tiaferna          #+#    #+#             */
-/*   Updated: 2023/11/16 13:07:08 by tiaferna         ###   ########.fr       */
+/*   Updated: 2023/11/16 23:21:38 by patatoss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@
 	return (0);	
 } */
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_vars *data, int x, int y, int color)
 {
 	char	*dst;
 
@@ -78,7 +78,6 @@ int shade_trgb(int color, double shade) //not wrking correctly yet
 
 int	close_win(int keycode, t_vars *vars)
 {
-	ft_printf("%d\n", keycode);
 	if (keycode == 65307)
 	{
 		mlx_destroy_window(vars->mlx, vars->win);
@@ -94,7 +93,7 @@ int	mouse_enter_hook(void)
 	return (0);
 }
 
-void drawCircle(t_data img, int xc, int yc, int x, int y) 
+void drawCircle(t_vars img, int xc, int yc, int x, int y) 
 { 
 	
     my_mlx_pixel_put(&img, xc+x, yc+y, 255); 
@@ -109,11 +108,11 @@ void drawCircle(t_data img, int xc, int yc, int x, int y)
   
 // Function for circle-generation 
 // using Bresenham's algorithm 
-void circleBres(t_data img, int xc, int yc, int r) 
+void circleBres(t_vars *img, int xc, int yc, int r) 
 {
     int x = 0, y = r; 
     int d = 3 - 2 * r; 
-    drawCircle(img, xc, yc, x, y); 
+    drawCircle(*img, xc, yc, x, y); 
     while (y >= x) 
     { 
         // for each pixel we will 
@@ -131,25 +130,30 @@ void circleBres(t_data img, int xc, int yc, int r)
         } 
         else
             d = d + 4 * x + 6; 
-        drawCircle(img, xc, yc, x, y); 
+        drawCircle(*img, xc, yc, x, y); 
         //delay(50); 
     } 
 } 
 
-int	move_right(int keycode, t_vars vars, t_data img, int xc, int yc, int r)
+int	move_object(int keycode, t_vars *vars)
 {
-	ft_printf("%d\n", keycode);
 	if (keycode ==  100)
-	{
-		circleBres(img, ++xc, ++yc, r);
-		mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	}
+        vars->xc += 10;
+	if (keycode ==  97)
+        vars->xc -= 10;
+	if (keycode ==  119)
+        vars->yc -= 10;
+	if (keycode ==  115)
+        vars->yc += 10;
+	vars->img = mlx_new_image(vars->mlx, 500, 500); // Clear the window
+	vars->addr = mlx_get_data_addr(vars->img, &vars->bits_per_pixel, &vars->line_length, &vars->endian);
+	circleBres(vars, vars->xc, vars->yc, vars->rad); // Redraw the circle with updated coordinates
+	mlx_put_image_to_window(vars->mlx, vars->win, vars->img, 0, 0); // Display the updated image
 	return (0);
 }
 
 int	main(void)
 {
-	t_data	img;
 	t_vars	vars;
 	int	x;
 	int	y;
@@ -163,52 +167,52 @@ int	main(void)
 	color = create_trgb(0, 255, 0, 0);
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, 500, 500, "so_long");
-	img.img = mlx_new_image(vars.mlx, 500, 500);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	// int	red = 255;
-	// int	green = 0;
-	// int	blue = 0;
-	/* while(1)
-	{
-		y = reset_y;
-		while (y <= 500)
-		{
-			x = reset_x;
-			while (x <= 500)
-			{
-				color = create_trgb(0, red, green, blue);
-				my_mlx_pixel_put(&img, x, y, color);
-				x++;
-			}
-			y++;
-		}
-		if (red > 0 && blue == 0)
-		{
-			green++;
-			red--;
-		}
-		if (green > 0 && red == 0)
-		{
-			blue++;
-			green--;
-		}
-		if (blue > 0 && green == 0)
-		{
-			red++;
-			blue--;
-		}
-		mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-		usleep(100000);
-		ft_printf("%d %d %d\n", red, green, blue);
-	} */
-	int xc = 250;
-	int yc = 250;
-	int r = 200;
-	circleBres(img, xc, yc, r);
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	mlx_hook(vars.win, 2, 1L<<0, move_right, &vars);
-	//mlx_hook(vars.win, 2, 1L<<0, close_win, &vars);
+	vars.img = mlx_new_image(vars.mlx, 500, 500);
+	vars.addr = mlx_get_data_addr(vars.img, &vars.bits_per_pixel, &vars.line_length,
+								&vars.endian);
+	int	red = 255;
+	int	green = 0;
+	int	blue = 0;
+	vars.xc = 250;
+	vars.yc = 250;
+	vars.rad = 200;
+	circleBres(&vars, vars.xc, vars.yc, vars.rad);
+	mlx_put_image_to_window(vars.mlx, vars.win, vars.img, 0, 0);
+	mlx_hook(vars.win, 2, 1L<<0, close_win, &vars);
+	mlx_hook(vars.win, 2, 1L<<0, move_object, &vars);
 	mlx_hook(vars.win, 6, 1L<<6, mouse_enter_hook, &vars);
 	mlx_loop(vars.mlx);
+	// while(1)
+	// {
+	// 	y = reset_y;
+	// 	while (y <= 500)
+	// 	{
+	// 		x = reset_x;
+	// 		while (x <= 500)
+	// 		{
+	// 			color = create_trgb(0, red, green, blue);
+	// 			my_mlx_pixel_put(&vars, x, y, color);
+	// 			x++;
+	// 		}
+	// 		y++;
+	// 	}
+	// 	if (red > 0 && blue == 0)
+	// 	{
+	// 		green++;
+	// 		red--;
+	// 	}
+	// 	if (green > 0 && red == 0)
+	// 	{
+	// 		blue++;
+	// 		green--;
+	// 	}
+	// 	if (blue > 0 && green == 0)
+	// 	{
+	// 		red++;
+	// 		blue--;
+	// 	}
+	// 	mlx_put_image_to_window(vars.mlx, vars.win, vars.img, 0, 0);
+	// 	usleep(100000);
+	// 	ft_printf("%d %d %d\n", red, green, blue);
+	// }
 }
